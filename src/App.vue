7161855b8f950f47ba702b1b81db7bf821d81aad
pieterj2000 <>
2022-02-27 01:18:10 +0100

    meuk plus todo

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,30 @@
         @canplaythrough="skipMandatoryCommercial"></audio>
 
 <button @click="this.$refs.radiolist.volgende()">volgende radio</button>
-<button @click="skipMandatoryCommercial(true)">skip verplichte commercial begin stream</button>
+<button @click="skipMandatoryCommercial($event, true)">skip verplichte commercial begin stream</button>
 <button @click="this.$refs.audio.pause()">pauze</button>
 <button @click="this.$refs.audio.play()">play</button>
+<button @click="newZender(this.zender)">skip naar live (reset)</button>
 
+<input type="range" id="volume" name="volume" min="0" max="100" v-model="volume" @input="this.$refs.audio.volume = this.volume / 100.0">
+
+
+
+<hr style="margin-top: 100px">
+<em>Todo:</em>
+<ul>
+    <li>Huidige nummer met API van zenders</li>
+    <li>de hele 'player' in een los component stoppen?</li>
+    <li>Opmaak/css, radiologo's etc..</li>
+    <li>Waveform van afgelopen x minuten met buffer die geanalyseerd wordt mooi canvas toevoegen</li>
+    <li>Die fourieranalyse en daar dan mee trainen of er commercial is of niet</li>
+    <li>resultaat van reclameanalyse toevoegen aan canvas</li>
+    <li>manier van trainen toevoegen en opslag (localstorage?)</li>
+    <li>toevoegen dat hij ook handelt automatisch (bijvoorbeeld popup met 3seconde delay)</li>
+    <li>super fancy dat je op de mooie canvas met de waveform ook kan schuiven en kan luisteren en repeat en dergelijke</li>
+    <li>oude trainwaarden inzien en wijzigen, of nieuwe toevoegen met bovenstaande functionaliteit</li>
+    <li>misschien al die fancy meuk achter een vinkje doen zodat het geen performence kost als je niet gebruikt</li>
+</ul>
 
 </template>
 
@@ -27,6 +47,7 @@ export default defineComponent({
         return {
             zender: {},
             skippedcommercial: false,
+            volume: 100
         }
     },
     computed: {
@@ -36,6 +57,7 @@ export default defineComponent({
     methods: {
         newZender(zender) {
             this.zender = zender;
+            this.$refs.audio.load();
             this.skippedcommercial = false;
         },
 
